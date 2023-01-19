d1654924fd11da08a1e7d3c7c539e12c60f834a1
ndragun92 <nemanjadragun92@gmail.com>
2023-01-19 21:36:46 +0100

    feat: add voice section

---
+++ components/section/Homepage/Voice/SectionHomepageVoice.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="bg-neutral-100">
+    <div class="container flex items-center py-40">
+      <div class="flex-1">
+        <h2 class="text-4xl font-bold">Where hanging out is easy</h2>
+        <p class="mt-10 text-lg">
+          Grab a seat in a voice channel when you’re free. Friends in your
+          server can see you’re around and instantly pop in to talk without
+          having to call.
+        </p>
+      </div>
+      <div class="flex-2">
+        <img class="ml-auto" src="/img/homepage/voice.svg" alt="" />
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,21 @@
   <div>
     <SectionHomepageHero />
     <SectionHomepageServers />
+    <SectionHomepageVoice />
   </div>
 </template>
 
 <script lang="ts">
 import SectionHomepageHero from "~/components/section/Homepage/Hero/SectionHomepageHero.vue";
 import SectionHomepageServers from "~/components/section/Homepage/Servers/SectionHomepageServers.vue";
+import SectionHomepageVoice from "~/components/section/Homepage/Voice/SectionHomepageVoice.vue";
 
 export default {
   name: "PageHomepage",
-  components: { SectionHomepageServers, SectionHomepageHero },
+  components: {
+    SectionHomepageVoice,
+    SectionHomepageServers,
+    SectionHomepageHero,
+  },
 };
 </script>
