2349a59bef73e202dde15f79742900831d373be8
ndragun92 <nemanjadragun92@gmail.com>
2023-01-19 21:40:26 +0100

    feat: add new moderation section

---
+++ components/section/Moderation/SectionHomepageModeration.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div class="container flex items-center py-32">
+      <div class="flex-2">
+        <img src="/img/homepage/moderation.svg" alt="" />
+      </div>
+      <div class="flex-1">
+        <h2 class="text-4xl font-bold">From few to a fandom</h2>
+        <p class="mt-10 text-lg">
+          Get any community running with moderation tools and custom member
+          access. Give members special powers, set up private channels, and
+          more.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <SectionHomepageHero />
     <SectionHomepageServers />
     <SectionHomepageVoice />
+    <SectionHomepageModeration />
   </div>
 </template>
 
@@ -10,10 +11,12 @@
 import SectionHomepageHero from "~/components/section/Homepage/Hero/SectionHomepageHero.vue";
 import SectionHomepageServers from "~/components/section/Homepage/Servers/SectionHomepageServers.vue";
 import SectionHomepageVoice from "~/components/section/Homepage/Voice/SectionHomepageVoice.vue";
+import SectionHomepageModeration from "~/components/section/Moderation/SectionHomepageModeration.vue";
 
 export default {
   name: "PageHomepage",
   components: {
+    SectionHomepageModeration,
     SectionHomepageVoice,
     SectionHomepageServers,
     SectionHomepageHero,
